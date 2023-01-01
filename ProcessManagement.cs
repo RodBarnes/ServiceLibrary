4829145261e762831cda3ed22088dc84aa90fb9a
advapp <advappsw@gmail.com>
2023-01-01 14:07:23 -0800

    Add project files.

---
+++ ProcessManagement.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Common
+{
+    public static class ProcessManagement
+    {
+        public static string RunCommand(string command, string workingDirectory = null)
+        {
+            try
+            {
+                var procStartInfo = new ProcessStartInfo("cmd", "/c " + command);
+
+                procStartInfo.RedirectStandardError = procStartInfo.RedirectStandardInput = procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                if (workingDirectory != null)
+                {
+                    procStartInfo.WorkingDirectory = workingDirectory;
+                }
+
+                var proc = new Process
+                {
+                    StartInfo = procStartInfo
+                };
+                proc.Start();
+
+                var sb = new StringBuilder();
+                proc.OutputDataReceived += delegate (object sender, DataReceivedEventArgs e)
+                {
+                    sb.AppendLine(e.Data);
+                };
+                proc.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs e)
+                {
+                    sb.AppendLine(e.Data);
+                };
+
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
+
+                return sb.ToString();
+            }
+            catch (Exception objException)
+            {
+                return $"Error in command: {command}, {objException.Message}";
+            }
+        }
+
+        public static void StopService(string serviceName, int timeoutMilliseconds)
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                var timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(StopService)} failed.", ex);
+            }
+        }
+
+        public static void StartService(string serviceName, int timeoutMilliseconds)
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                var timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(StartService)} failed.", ex);
+            }
+        }
+    }
+}
